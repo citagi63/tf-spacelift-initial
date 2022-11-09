1d585903bbd9a5da512d0744dc0199f877fdf7fd
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-09 17:53:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,15 +65,15 @@ resource "aws_security_group" "mtc_sg" {
   }
 }
 
-resource "aws_key_pair" "mtc_auth" {
-  key_name   = "west"
-  public_key = file("C:\Users\ChandrakantI\Downloads/west.pem")
-}
+#resource "aws_key_pair" "mtc_auth" {
+ # key_name   = "west"
+  #public_key = file("C:\Users\ChandrakantI\Downloads/west.pem")
+#}
 
 resource "aws_instance" "dev_node" {
     instance_type = "t2.micro"
     ami = data.aws_ami.server_ami.id
-    key_name = aws_key_pair.mtc_auth.id 
+    key_name = "west" 
     vpc_security_group_ids = [aws_security_group.mtc_sg.id]
     subnet_id = aws_subnet.mtc_public_subnet.id
     user_data = file("userdata.tpl")
